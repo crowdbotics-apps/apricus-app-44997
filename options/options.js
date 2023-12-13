f21a631cf0e9edb8e23e2a7814382305b8f93c08
Crowdbotics <team@crowdbotics.com>
2023-12-13 20:40:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "apricus-app-44997",
+  url: "https://apricus-app-44997.botics.co",
+  api: "https://apricus-app-44997.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
